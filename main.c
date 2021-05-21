45a288a1391634183dd318ce79a0f4c779217ee4
thetruezau <80555837+thetruezau@users.noreply.github.com>
2021-05-21 16:13:48 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 #include "libs/list.h"
 #include "libs/hash.h"
 
+char *filename = "lusiadas.txt";
 char *delim = " .,;?!()[]:\"\n-\'";
 
 void next_word(FILE *fp, char *word) {  
@@ -28,36 +29,55 @@ void next_word(FILE *fp, char *word) {
   word[i] = 0;
 }
 
+void t9_autocomplete(hash_table table, char *t9) {
+  int len = strlen(t9);
+  list l = table[hash(t9)];
+
+  while((l = l->next) != NULL) {
+    if(utf8_strlen(l->key->str) == len)
+      printf(" %s", l->key->str);
+  }
+  printf("\n");
+}
+
 int main() {
+  FILE *fp = fopen(filename, "r");
+  
   hash_table table = new_table();
   int words = 0;
   
   while(1) {
     char* word = malloc(20);
-    next_word(stdin, word);
+    next_word(fp, word);
     if(word[0] == '\0') break;
     insert_table(table, word);
     words++;
   }
+  
+  /* int cnt = 0, max = 0; */
+  /* for(int i = 0; i < M; i++) { */
+  /*   cnt += table[i]->next == NULL ? 1 : 0; */
 
-  int cnt = 0, max = 0;
-  for(int i = 0; i < M; i++) {
-    cnt += table[i]->next == NULL ? 1 : 0;
+  /*   list l = table[i]; */
+  /*   int size = 0; */
+  /*   while((l = l->next) != NULL) */
+  /*     size++; */
 
-    list l = table[i];
-    int size = 0;
-    while((l = l->next) != NULL)
-      size++;
+  /*   max = max < size ? size : max; */
+  /* } */
+  
+  /* printf("words %d\n%d/%d = %.2f\n", words, (M-cnt), M, (float)(M-cnt)/M); */
+  /* printf("max: %d\n\n", max); */
+  
+  /* print_list(table[hash("6")]); */
 
-    max = max < size ? size : max;
+  while(1) {
+    char str[20];
+    scanf("%s", str);
+    t9_autocomplete(table, str);
   }
-
-  /*
-  printf("words %d\n%d/%d = %.2f\n", words, (M-cnt), M, (float)(M-cnt)/M);
-  printf("max: %d\n", max);
-  */
-
-  print_list(table[hash("5652")]);
+  
+  fclose(fp);
   
   return 0;
 }
